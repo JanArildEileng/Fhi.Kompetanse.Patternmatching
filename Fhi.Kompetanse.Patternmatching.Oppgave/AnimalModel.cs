bd05d7aeb6022cc2750ce6ebc0f4674dfb93d218
janarild.eileng <janarild.eileng@fhi.no>
2025-05-20 08:23:57 +0200

    Add project files.

---
+++ Fhi.Kompetanse.Patternmatching.Oppgave/AnimalModel.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Fhi.Kompetanse.Patternmatching.Oppgave;
+
+abstract class Animal
+{
+}
+
+
+internal class Dog : Animal
+{
+    public string? Breed { get;  set; }
+}
+
+internal class Labrador : Dog
+{
+    public Labrador()
+    {
+         Breed="Labrador";
+    }
+}
+
+internal class Cat : Animal
+{
+    public string Color { get; internal set; }
+    public string Breed { get; internal set; }
+    public int Weight { get; set; } = 4;
+
+    public bool IsBigCat()
+    {
+        return Weight > 4;
+    }
+}
+
+internal class Crocodile : Animal
+{
+}
---
+++ Fhi.Kompetanse.Patternmatching.Oppgave/PatternSwitcher.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Fhi.Kompetanse.Patternmatching.Oppgave;
+
+internal class PatternSwitcher
+{
+    internal string GetAnimalName(Animal animal)
+    {
+        //NO IF!
+        return animal switch 
+        {
+            Labrador dog => "Labrador",             
+            Dog dog => "Dog",
+
+            Cat cat and {Color:"Blue" }  when cat.IsBigCat()=>"BigBlueCat",
+            Cat cat and {Color:"Blue" } =>"BlueCat",
+          
+            Cat cat and {Breed:"RussianBlue" }=>"RussianBlueCat",
+            Cat cat when cat.IsBigCat()  => "BigCat",
+         
+            Cat cat => "Cat",
+            _ => throw new NotImplementedException()      
+        };  
+    }
+
+}
