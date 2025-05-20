319370a31e946a77f5124b8d3e03b8eab664c36f
janarild.eileng <janarild.eileng@fhi.no>
2025-05-20 08:28:01 +0200

    Kompetansedag

--- Fhi.Kompetanse.Patternmatching.Oppgave/PatternSwitcher.cs
+++ Fhi.Kompetanse.Patternmatching.Oppgave/PatternSwitcher.cs
@@ -6,20 +6,7 @@ internal class PatternSwitcher
     internal string GetAnimalName(Animal animal)
     {
         //NO IF!
-        return animal switch 
-        {
-            Labrador dog => "Labrador",             
-            Dog dog => "Dog",
-
-            Cat cat and {Color:"Blue" }  when cat.IsBigCat()=>"BigBlueCat",
-            Cat cat and {Color:"Blue" } =>"BlueCat",
-          
-            Cat cat and {Breed:"RussianBlue" }=>"RussianBlueCat",
-            Cat cat when cat.IsBigCat()  => "BigCat",
-         
-            Cat cat => "Cat",
-            _ => throw new NotImplementedException()      
-        };  
+        throw new Exception("Not done");    
     }
 
 }
